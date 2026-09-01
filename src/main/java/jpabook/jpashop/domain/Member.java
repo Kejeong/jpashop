3ecd924a903220074a3b875dc7f212076b83b911
kimeunjeong <keunjeong613@gmail.com>
2026-09-01 15:45:27 +0900

    프로젝트 환경설정

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private Address address;
+}
