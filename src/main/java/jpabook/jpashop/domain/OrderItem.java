24f6895d37003cbe482c94edee922c735b9fc175
kimeunjeong <keunjeong613@gmail.com>
2026-09-03 17:53:32 +0900

    주문서비스 구현

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,13 +2,16 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class OrderItem {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderItem {  // 주문 상품
     @Id
     @GeneratedValue
     @Column(name = "order_item_id")
@@ -25,4 +28,28 @@ public class OrderItem {
     private int orderPrice;  // 주문 가격
 
     private int count;  // 주문 수량
+
+    // 비즈니스 로직
+    public void cancel() {  // 재고수량을 원복해준다
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+
+    // 생성 메서드
+    /**
+     * 주문생성
+     */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
 }
